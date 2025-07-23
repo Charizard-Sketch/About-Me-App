bb9205a6ef21ba3a9e46a31edcc1b5f4ba2a05a8
Montserrat <jaureguimontse16@gmail.com>
2025-07-23 13:02:38 -0700

    added text and added an image

--- About Me App/ContentView.swift
+++ About Me App/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Montserrat Jauregui")
+            Image("Gravity Falls")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
         }
         .padding()
     }
