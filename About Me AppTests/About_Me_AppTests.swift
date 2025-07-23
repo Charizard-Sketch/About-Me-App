22c5d8c00f64453104d241702fe0f4d126724f97
Montserrat <jaureguimontse16@gmail.com>
2025-07-23 12:55:38 -0700

    Initial Commit

---
+++ About Me AppTests/About_Me_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  About_Me_AppTests.swift
+//  About Me AppTests
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import Testing
+@testable import About_Me_App
+
+struct About_Me_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
